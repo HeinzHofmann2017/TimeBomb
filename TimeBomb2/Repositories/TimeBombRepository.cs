c450d8c813d38ec795ce3f7a24f7a6a8b4cefdd2
Heinz Hofmann <heinz.hofmann@zuehlke.com>
2020-11-20 15:18:17 +0100

    Created Repository and First Method

---
+++ TimeBomb2/Repositories/TimeBombRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using TimeBomb.Data;
+using TimeBomb2.Data.Access;
+
+namespace TimeBomb2.Repositories
+{
+    public class TimeBombRepository
+    {
+        public Guid CreateGameAndGetItsId()
+        {
+            var game = new Game()
+            {
+                GameId = Guid.NewGuid()
+            };
+            var store = DocumentStoreHolder.Store;
+            using var session = store.OpenSession();
+            session.Store(game);
+            return game.GameId;
+        }
+    }
+}
