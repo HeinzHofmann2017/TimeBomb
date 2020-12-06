8895c9453b510f827196037e27272e2136bc37cb
Heinz Hofmann <heinz.hofmann@zuehlke.com>
2020-12-06 16:32:14 +0100

    Made Constructor, to automatically initialize Game-Dto's

--- TimeBomb2/Data/Dtos/OtherPlayerDto.cs
+++ TimeBomb2/Data/Dtos/OtherPlayerDto.cs
@@ -5,6 +5,13 @@ namespace TimeBomb2.Data.Dtos
 {
     public class OtherPlayerDto
     {
+        public OtherPlayerDto(Player player)
+        {
+            Name = player.Name;
+            HoldsNipper = player.HoldsNipper;
+            NumberOfHiddenPlayCards = player.HiddenPlayCards.Count;
+        }
+        
         public string Name { get; set; }
         public bool HoldsNipper { get; set; }
         public int NumberOfHiddenPlayCards { get; set; }
--- TimeBomb2/Services/PlayerSpecificGameDto.cs
+++ TimeBomb2/Services/PlayerSpecificGameDto.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using TimeBomb.Data;
+using TimeBomb2.Data;
 using TimeBomb2.Data.Dtos;
 
 namespace TimeBomb2.Services
 {
     public class PlayerSpecificGameDto
     {
+        public PlayerSpecificGameDto(Game game, Guid playerId)
+        {
+            GameId = game.GameId;
+            OwnPlayer = game.Players.First(p => p.PlayerId == playerId);
+            RevealedPlayCards = game.RevealedPlayCards;
+            Started = game.Started;
+            OtherPlayers = GetOtherPlayers(game, playerId);
+        }
+        
         public Guid GameId { get; set; }
         public List<OtherPlayerDto> OtherPlayers { get; set; }
         public Player OwnPlayer { get; set; }
         public List<RevealedPlayCard> RevealedPlayCards { get; set; }
         public bool Started { get; set; }
+
+        private List<OtherPlayerDto> GetOtherPlayers(Game game, Guid playerId)
+        {
+            var otherPlayerDtos = new List<OtherPlayerDto>();
+            game.Players.ForEach(p =>
+            {
+                if (p.PlayerId != playerId)
+                {
+                    otherPlayerDtos.Add(new OtherPlayerDto(p));
+                }
+            });
+            return otherPlayerDtos;
+        }
     }
 }
