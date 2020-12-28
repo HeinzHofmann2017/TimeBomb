eed5a3b4d77ebfd27b56f9f0e97bf5fe6ed0cfea
Heinz Hofmann <heinz.hofmann@zuehlke.com>
2020-12-28 16:47:25 +0100

    realized, that static classes cannot be mocked...

---
+++ TimeBomb2UnitTests/Services/TimeBombServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using TimeBomb2.Repositories;
+
+namespace TimeBomb2UnitTests.Services
+{
+    [TestClass]
+    public class TimeBombServiceTests
+    {
+        [TestMethod]
+        public void TooManyPlayersRegistered_RegisterNewPlayer_ReturnsNull()
+        {
+            // Arrange
+            var timeBombRepositoryMock = new Mock<TimeBombRepository>();
+        }
+        
+    }
+}
