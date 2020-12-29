cf706f611a574b582b4f58f6230afbf89487763e
Heinz Hofmann <heinz.hofmann@zuehlke.com>
2020-12-29 17:18:06 +0100

    Created Timebombcontroller

---
+++ TimeBomb2/Controllers/TimeBombController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TimeBomb2.Repositories;
+using TimeBomb2.Services;
+
+namespace TimeBomb2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TimeBombController : ControllerBase
+    {
+        private readonly ILogger<TimeBombController> _logger;
+        private readonly TimeBombService _timeBombService;
+
+        public TimeBombController(ILogger<TimeBombController> logger)
+        {
+            _logger = logger;
+            _timeBombService = new TimeBombService(new TimeBombRepository());
+        }
+
+        [HttpGet]
+        public Guid CreateGame()
+        {
+            return _timeBombService.CreateGame();
+        }
+    }
+}
