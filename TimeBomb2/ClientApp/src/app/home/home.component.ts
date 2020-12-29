0a581cdb2e88315a80d9ca97fe995c176f2a2130
Heinz Hofmann <heinz.hofmann@zuehlke.com>
2020-12-29 23:29:55 +0100

    Create new game in backend, when button is hit :)

--- TimeBomb2/ClientApp/src/app/home/home.component.ts
+++ TimeBomb2/ClientApp/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
 import { Router } from '@angular/router';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-home',
@@ -7,12 +8,20 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent {
   public gameId: string;
+  private http: HttpClient;
+  private baseUrl: string;
 
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    this.http = http;
+    this.baseUrl = baseUrl;
+  }
   createNewGame() {
     console.log("Method createNewGame was called")
-    // create new game
-    console.log("New Game with Id '" + this.gameId + "' has been created")
-    // route to registerplayer-component
+    this.http.get<string>(this.baseUrl + 'timebomb/creategame').subscribe(result => {
+           this.gameId = result;
+           console.log("New Game with Id '" + this.gameId + "' has been created")
+           // route to registerplayer-component
+         }, error => console.error(error));
   }
 
   joinExistingGame(){
