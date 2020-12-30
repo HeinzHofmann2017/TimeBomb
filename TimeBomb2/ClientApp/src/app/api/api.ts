d283b91d2962abce2f36f7de22ecd5aeb33b6a16
Heinz Hofmann <heinz.hofmann@zuehlke.com>
2020-12-30 03:14:04 +0100

    Show Names of Enums in Frontend instead of just numbers

--- TimeBomb2/ClientApp/src/app/api/api.ts
+++ TimeBomb2/ClientApp/src/app/api/api.ts
@@ -3,11 +3,20 @@
   Swat = 1
 }
 
+export let RoleCardsDisplay: { [index: number]: string} = {};
+RoleCardsDisplay[roleCard.Terrorist] = "Terrorist";
+RoleCardsDisplay[roleCard.Swat] = "SWAT"
+
 export enum playCard {
   Bomb =0,
   Success = 1,
   Safe = 2
 }
+
+export let PlayCardsDisplay: { [index: number]: string} = {};
+PlayCardsDisplay[playCard.Bomb] = "Bomb";
+PlayCardsDisplay[playCard.Success] = "Success";
+PlayCardsDisplay[playCard.Safe] = "Safe";
 
 export class player {
   playerId: string;
--- TimeBomb2/ClientApp/src/app/play-field/play-field.component.ts
+++ TimeBomb2/ClientApp/src/app/play-field/play-field.component.ts
@@ -1,7 +1,7 @@
 import {Component, Inject} from '@angular/core';
 import {Router, ActivatedRoute} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
-import {playerSpecificGameDto,otherPlayerDto, player, revealedPlayCard, playCard, roleCard} from "../api/api"
+import {playerSpecificGameDto, PlayCardsDisplay, RoleCardsDisplay, player, revealedPlayCard, playCard, roleCard} from "../api/api"
 
 @Component({
   selector: 'app-play-field',
@@ -13,12 +13,16 @@ export class PlayFieldComponent {
   http: HttpClient;
   baseUrl: string;
   playerSpecificGameDto: playerSpecificGameDto;
+  playCardsDisplay: { [index: number]: string};
+  roleCardsDisplay: { [index: number]: string};
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, private router:Router, private route: ActivatedRoute){
     this.http = http;
     this.baseUrl = baseUrl;
     this.gameId = this.route.snapshot.paramMap.get('gameId');
     this.playerId = this.route.snapshot.paramMap.get('playerId');
+    this.playCardsDisplay = PlayCardsDisplay;
+    this.roleCardsDisplay = RoleCardsDisplay;
 
     this.http.get<playerSpecificGameDto>(this.baseUrl + 'timebomb/getactualgamestate?gameId='+this.gameId+'&playerId='+this.playerId)
       .subscribe(resultingPlayerSpecificGameDto => {
